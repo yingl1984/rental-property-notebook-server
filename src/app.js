93c0c8b8fc35500065c197ce6fef3b62636b4d0e
Ying Liu <yingl1984@gmail.com>
2021-04-15 18:01:50 -0700

    cors

--- src/app.js
+++ src/app.js
@@ -15,11 +15,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(
-    cors({
-        origin: CLIENT_URL
-    })
-);
+
 
 app.use('/api/notes', notesRouter);
 
